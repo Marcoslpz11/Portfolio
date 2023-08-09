cb4c218eedb1cc73ee90137a3f36dd55b753952a
LOPEZ GARCIA MARCOS <135575069+Marcoslpz11@users.noreply.github.com>
2023-08-09 21:36:15 +0900

    Update contact-form-handler.php

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,21 +1,20 @@
 <?php
-    $name =$_POST['name'];
+    $name = $_POST['name'];
     $visitor_email = $_POST['email'];
     $message = $_POST['message'];
 
-    $email_from ='lopezgmrcs@gmail.com';
+    $email_from = 'lopezgmrcs@gmail.com';
     $email_subject = "New form submission";
-    $email_body = "User Name: $name.\n".
-                    "User Email: $visitor_email.\n".
-                        "User Message: $message.\n";
+    $email_body = "User Name: $name.\n" .
+                  "User Email: $visitor_email.\n" .
+                  "User Message: $message.\n";
 
     $to = "lopezgmrcs@gmail.com";
 
-    $headers="From: $email_from \r\n";
-    $headers .="Replay-To: $visitor_email \r\n";
+    $headers = "From: $email_from \r\n";
+    $headers .= "Reply-To: $visitor_email \r\n";
 
-    mail($to,$email_subject,$email_body,$headers);
+    mail($to, $email_subject, $email_body, $headers);
 
     header("Location: index.html");
-
 ?>
